efdc1e2113b6234f98f680e6a39fed7a504dea90
PROGRAMING_JAY <140893550+jaydholi@users.noreply.github.com>
2024-09-05 14:54:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// JavaScript for Modal and QR Code Interaction
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById('appointment-modal');
+    var closeButton = document.querySelector('.close-button');
+
+    // Open Modal
+    function openBookingForm() {
+        modal.style.display = 'flex';
+    }
+
+    // Close Modal
+    closeButton.onclick = function () {
+        closeBookingForm();
+    };
+
+    // Close modal when clicking outside of the modal content
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            closeBookingForm();
+        }
+    };
+
+    function closeBookingForm() {
+        modal.style.display = 'none';
+    }
+});
 
